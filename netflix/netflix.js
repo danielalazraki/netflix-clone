255f45cdd266d68f704db16d16f0316bdff4a50e
danielalazraki <80274572+danielalazraki@users.noreply.github.com>
2021-04-26 12:35:59 -0400

    Add files via upload

---
+++ netflix/netflix.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    document.getElementById("wid").innerHTML = "The information collected by Google reCAPTCHA is subject to the Google Privacy Policy and Terms of Service, and is used for providing, maintaining, and improving the reCAPTCHA service and for general security purposes (it is not used for personalized advertising by Google).";
+  }
+  var person = prompt('hello');
+  
